4659b7e3a8e61d2e9271b48eb19fe996454d7131
lili.2 <lili.2@bytedance.com>
2025-07-08 21:45:07 +0800

    first version easydb

--- client_test.go
+++ client_test.go
@@ -34,53 +34,6 @@ func TestClient(t *testing.T) {
 
 	sendSql(conn, "SELECT * FROM lili WHERE a = 1;")
 
-	//
-	//
-	//createTable := "create table lili (a int, b int, c int);"
-	//_, err = conn.Write([]byte(createTable))
-	//if err != nil {
-	//	fmt.Println("发送失败:", err)
-	//	return
-	//}
-	//fmt.Printf("已发送: %s \n", createTable)
-	//
-	//time.Sleep(1 * time.Second) // 每秒发送一次，可根据需求调整
-	//
-	//// 读取服务器响应
-	//msg := make([]byte, 1024)
-	//lenc, _ := conn.Read([]byte(msg))
-	//fmt.Printf("服务器响应:\n %s \n", msg[0:lenc])
-	//
-	//descTable := "desc lili;"
-	//_, err = conn.Write([]byte(descTable))
-	//if err != nil {
-	//	fmt.Println("发送失败:", err)
-	//	return
-	//}
-	//fmt.Printf("已发送: %s \n", descTable)
-	//
-	//time.Sleep(1 * time.Second) // 每秒发送一次，可根据需求调整
-	//
-	//// 读取服务器响应
-	//msg = make([]byte, 1024)
-	//lenc, _ = conn.Read([]byte(msg))
-	//fmt.Printf("服务器响应:\n %s \n", msg[0:lenc])
-	//
-	////持续发送数据
-	//for i := 0; ; i++ {
-	//	msg := fmt.Sprintf("select * from lili where a =  %d;", i)
-	//	_, err := conn.Write([]byte(msg))
-	//	if err != nil {
-	//		fmt.Println("发送失败:", err)
-	//		return
-	//	}
-	//	fmt.Printf("已发送: %s \n", msg)
-	//	time.Sleep(1 * time.Second) // 每秒发送一次，可根据需求调整
-	//	newmsg := make([]byte, 1024)
-	//	lenc, _ = conn.Read([]byte(newmsg))
-	//	fmt.Printf("服务器响应:\n %s \n", newmsg[0:lenc])
-	//
-	//}
 }
 
 func sendSql(conn net.Conn, sql string) {
